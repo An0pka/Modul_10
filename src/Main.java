0a6d846a3593fa8517e71f60e7065f768a03a895
An0pka <an0pka32@gmail.com>
2023-07-09 18:10:40 +0300

    Modul 10, task 1, done

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,18 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("file.txt"));
-        String phoneNumber;
-        while (bufferedReader.ready()){
-            phoneNumber = bufferedReader.readLine();
-            if (phoneNumber.matches("\\d{3}-\\d{3}-\\d{4}")
-               ||phoneNumber.matches("\\(\\d{3}\\) \\d{3}-\\d{4}")) {
-               System.out.println(phoneNumber);
+    public static void main(String[] args){
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("file.txt"))){
+            String phoneNumber;
+            while (bufferedReader.ready()){
+                phoneNumber = bufferedReader.readLine();
+                if (phoneNumber.matches("\\d{3}-\\d{3}-\\d{4}")
+                        ||phoneNumber.matches("\\(\\d{3}\\) \\d{3}-\\d{4}")) {
+                    System.out.println(phoneNumber);
+                }
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
